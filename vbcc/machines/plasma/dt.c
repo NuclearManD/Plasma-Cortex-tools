8547f32d6b4018bd3edab37659309e887136281d
NuclearManD <dylanbrophy@gmail.com>
2018-08-17 19:24:20 -0700

    FIXED THE COMPILER YESSSSSSS

--- vbcc/machines/plasma/dt.c
+++ vbcc/machines/plasma/dt.c
@@ -1,74 +1,6 @@
 
-
-/* Machine generated file. DON'T TOUCH ME! */
-
-
 #include "dt.h"
 
-#undef DTTTYPE
-#define DTTTYPE dt3t
-#undef DTFTYPE
-#define DTFTYPE dt3f
-dt3t dtcnv3f(dt3f
-#include "dtswap16f.c"
-dt3f dtcnv3t(dt3t
-#include "dtswap16t.c"
-#undef DTTTYPE
-#define DTTTYPE dt4t
-#undef DTFTYPE
-#define DTFTYPE dt4f
-dt4t dtcnv4f(dt4f
-#include "dtswap16f.c"
-dt4f dtcnv4t(dt4t
-#include "dtswap16t.c"
-#undef DTTTYPE
-#define DTTTYPE dt5t
-#undef DTFTYPE
-#define DTFTYPE dt5f
-dt5t dtcnv5f(dt5f
-#include "dtswap32f.c"
-dt5f dtcnv5t(dt5t
-#include "dtswap32t.c"
-#undef DTTTYPE
-#define DTTTYPE dt6t
-#undef DTFTYPE
-#define DTFTYPE dt6f
-dt6t dtcnv6f(dt6f
-#include "dtswap32f.c"
-dt6f dtcnv6t(dt6t
-#include "dtswap32t.c"
-#undef DTTTYPE
-#define DTTTYPE dt7t
-#undef DTFTYPE
-#define DTFTYPE dt7f
-dt7t dtcnv7f(dt7f
-#include "dtswap32f.c"
-dt7f dtcnv7t(dt7t
-#include "dtswap32t.c"
-#undef DTTTYPE
-#define DTTTYPE dt8t
-#undef DTFTYPE
-#define DTFTYPE dt8f
-dt8t dtcnv8f(dt8f
-#include "dtswap32f.c"
-dt8f dtcnv8t(dt8t
-#include "dtswap32t.c"
-#undef DTTTYPE
-#define DTTTYPE dt9t
-#undef DTFTYPE
-#define DTFTYPE dt9f
-dt9t dtcnv9f(dt9f
-#include "dtswap32f.c"
-dt9f dtcnv9t(dt9t
-#include "dtswap32t.c"
-#undef DTTTYPE
-#define DTTTYPE dt10t
-#undef DTFTYPE
-#define DTFTYPE dt10f
-dt10t dtcnv10f(dt10f
-#include "dtswap32f.c"
-dt10f dtcnv10t(dt10t
-#include "dtswap32t.c"
 #undef DTTTYPE
 #define DTTTYPE dt11t
 #undef DTFTYPE
@@ -93,11 +25,3 @@ dt13t dtcnv13f(dt13f
 #include "dtswap64f.c"
 dt13f dtcnv13t(dt13t
 #include "dtswap64t.c"
-#undef DTTTYPE
-#define DTTTYPE dt14t
-#undef DTFTYPE
-#define DTFTYPE dt14f
-dt14t dtcnv14f(dt14f
-#include "dtswap32f.c"
-dt14f dtcnv14t(dt14t
-#include "dtswap32t.c"
